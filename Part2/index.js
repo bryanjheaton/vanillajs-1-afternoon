f614f7fb1faabbbd98dfd86b2b0321988eb0e149
bryanjheaton <bheaton8604@gmail.com>
2019-03-04 17:04:48 -0700

    Updated files for afternoon practice

---
+++ Part2/index.js
@@ -0,0 +1,21 @@
+console.log("The house always wins!")
+
+var id = document.getElementById("idInput")
+var styleAtt = document.getElementById("colorInput")
+
+console.log(id)
+console.log(styleAtt)
+
+
+
+
+
+
+
+// function setCard() {
+//     const card = document.getElementById(id.value)
+//     console.log(card)
+//     card.style.color = styleAtt.value
+// }
+
+
